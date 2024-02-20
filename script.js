d197bf2a9484026b082b99316b5255f0f23f45fc
Nguyen Minh <minh47857@gmail.com>
2024-02-21 01:02:08 +0700

    feat: shop page

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 // Script for navigation bar
+const bar = document.getElementById('bar');
+const closeBtn = document.getElementById('close');
+const nav = document.getElementById('navbar');
+if(bar) {
+    bar.onclick = () => {
+        nav.style.right = 0;
+    }
+}
+
+if(closeBtn) {
+    closeBtn.onclick = () => {
+        nav.style.right = '-308px';
+    }
+}
